2ddbaf84e3be027a7991c5187fc5224ab99f321f
applejuice <applejuiceprogame@gmail.com>
2025-07-25 16:38:24 +0800

    ouiouioui

--- public/script.js
+++ public/script.js
@@ -5,3 +5,7 @@ document.getElementById('btn-mtr')?.addEventListener('click', () => {
 document.getElementById('btn-minibus')?.addEventListener('click', () => {
   window.location.href = '/home/applejuice/team-c-deaf-project/public/minibus.html';
 });
+document.getElementById('btn-bus')?.addEventListener('click', () => {
+  window.location.href = '/home/applejuice/team-c-deaf-project/public/bus.html';
+});
+
